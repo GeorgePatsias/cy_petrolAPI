4d2ba899fb81779516cbf8aff25b635070ba643d
George Patsias <george_patsias@hotmail.co.uk>
2020-12-11 10:10:56 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 API_KEY = "1"
 FLASK_SECRET_KEY = "aT}C2./}5rY*>h2f?=*F)j6}n=/Bf?V#[FR56BgN[CrQ8<.N!,dGZJw?JuBScykG=9Hn~+'-V-%kLAj9!dW!$yV)apU)FeUf#\@y"
 
-CHROMEDRIVER = "/home/george/Desktop/projects/cy_petrolAPI/chromedriver"
+CHROMEDRIVER = "/path/to/chromedriver"
 
 MONGO_CONNECTION = "mongodb://localhost:27017/"
 MONGO_DB = "my_information3"
